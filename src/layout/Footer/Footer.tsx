4e1b0d8810213bf138e708d099e50b55e517b067
Karl Cereno <kagc.dev@gmail.com>
2023-02-09 13:17:13 -0500

    Make footer component"

---
+++ src/layout/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+      <div>
+        <p>Karl Cereno 2020</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/layout/Layout/Layout.tsx
+++ src/layout/Layout/Layout.tsx
@@ -1,4 +1,5 @@
 import Navbar from '../Navbar/Navbar';
+import Footer from '../Footer/Footer';
 
 interface Props {
   children: React.ReactNode;
@@ -6,13 +7,15 @@ interface Props {
 
 function Layout({ children }: Props) {
   return (
-    <>
+    <div className="app-container min-h-screen flex flex-col">
       <Navbar />
-      <main className="flex justify-center">
-        <div className="w-[1300px]">{children}</div>
+      <main className="flex-1">
+        <div className="main-content-container flex justify-center">
+          <div className="w-[1300px]">{children}</div>
+        </div>
       </main>
-      {/* footer */}
-    </>
+      <Footer />
+    </div>
   );
 }
 
