2e78cffd25262c55b2d6c72163072cca61786a68
Karl Cereno <kagc.dev@gmail.com>
2023-02-15 13:18:31 -0500

    Make new habit card for HabitBoard component

--- src/layout/Layout/Layout.tsx
+++ src/layout/Layout/Layout.tsx
@@ -1,6 +1,4 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable react/self-closing-comp */
-/* eslint-disable no-lone-blocks */
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
 
@@ -10,34 +8,12 @@ interface Props {
 
 function Layout({ children }: Props) {
   return (
-    <div className="app-container min-h-screen  bg-base-100">
+    <div className="flex flex-col min-h-screen">
       <Navbar />
-      <main className="flex justify-center">
-        <div className="indicator">
-          <span className="indicator-item indicator-middle indicator-end badge badge-secondary">
-            <button type="button">+</button>
-          </span>
-
-          <div className="grid  bg-base-300 place-items-center my-10">
-            {children}
-          </div>
-        </div>
-      </main>
+      <main className="flex-1 flex justify-center bg-neutral">{children}</main>
       <Footer />
     </div>
   );
 }
 
 export default Layout;
-
-{
-  /* <div className="indicator">
-          <span className="indicator-item indicator-middle badge badge-secondary">
-            <button type="button">+</button>
-          </span>
-
-          <div className="main-content-container flex justify-center">
-            <div className="w-[1300px]">{children}</div>
-          </div>
-        </div> */
-}
