084641cdf3e904cedaf82ba2199a7b36688b0b1f
Karl Cereno <kagc.dev@gmail.com>
2023-02-19 20:19:38 -0500

    Finish UI

--- src/components/HabitCard/HabitCard.tsx
+++ src/components/HabitCard/HabitCard.tsx
@@ -1,46 +1,54 @@
+/* eslint-disable react/button-has-type */
 import React from 'react';
 
 function HabitCard() {
   return (
-    <div className="card w-2/5 grow card-compact bg-base-200 shadow-xl">
-      <div className="card-body">
-        {/* Streak Details */}
-        <div className="hidden mobile-medium:flex justify-between">
-          <div className="">
-            <p>Longest: 4</p>
-          </div>
-          <div className="">
-            <p>Current: 2</p>
+    <div className="card w-2/5 grow card-compact bg-base-200 shadow-xl ">
+      <div className="flex justify-end">
+        <button className="btn btn-ghost text-accent text-center text-xs">
+          Select
+        </button>
+      </div>
+      <div className="py-4 tablet:py-6 max:py-8">
+        {/* Habit Details */}
+        <div className="text-center">
+          <h1 className="text-4xl tablet:text-5xl">📝</h1>
+          <h2 className="text-xl font-bold mobile-medium:text-2xl tablet:text-3xl">
+            Journal
+          </h2>
+          <p className="text-gray-400">Once A Day</p>
+          {/* Streak Details */}
+          <div className="hidden mobile-medium:flex justify-center gap-4 ">
+            <div className="">
+              <p>Current: 2</p>
+            </div>
+            <div className="">
+              <p>Longest: 4</p>
+            </div>
           </div>
         </div>
-        {/* Habit Details */}
-        <div className="text-center my-4">
-          <h1 className="text-4xl">📝</h1>
-          <h2 className="text-xl font-bold mobile-medium:text-2xl">Journal</h2>
-          <p className="text-gray-500">Once A Day</p>
-          {/* Habit Progress */}
-          <div className=" mt-4">
-            <p>1/2</p>
-            <progress
-              className="progress progress-primary"
-              value="70"
-              max="100"
-            />
-          </div>
-          <div className="progress-buttons flex justify-center gap-1 mt-4">
-            <button
-              type="button"
-              className="btn btn-square btn-outline  btn-sm"
-            >
-              -
-            </button>
-            <button
-              type="button"
-              className="btn btn-square  btn-outline  btn-sm "
-            >
-              +
-            </button>
-          </div>
+        {/* Habit Progress */}
+        <div className="mt-5 text-center">
+          <p>1/2</p>
+          <progress
+            className="progress progress-primary w-3/4"
+            value="70"
+            max="100"
+          />
+        </div>
+        <div className="progress-buttons flex justify-center gap-1 tablet:gap-3 mt-4">
+          <button
+            type="button"
+            className="btn btn-square btn-outline btn-sm tablet:btn-md"
+          >
+            -
+          </button>
+          <button
+            type="button"
+            className="btn btn-square  btn-outline btn-sm tablet:btn-md"
+          >
+            +
+          </button>
         </div>
       </div>
     </div>
--- src/layout/Navbar/Navbar.tsx
+++ src/layout/Navbar/Navbar.tsx
@@ -2,22 +2,26 @@
 
 function Navbar() {
   return (
-    <>
+    <div className="flex justify-center z-10">
       {/* Top Nav */}
-      <div className="flex justify-center z-10">
-        {/* Top Nav */}
-        <div className="w-[900px]">
-          <div className="flex items-center mx-2 justify-between tablet:mx-4  max:mx-0">
-            <div>
-              <p className="py-3 font-bold normal-case text-xl text-center tablet:text-left ">
-                StreakTracker
-              </p>
-            </div>
-            <div className="">New Habit</div>
+      <div className="w-[900px]">
+        <div className="flex items-center mx-2 justify-between tablet:mx-4  max:mx-0">
+          <div>
+            <p className="py-3 font-bold normal-case text-xl text-center tablet:text-left ">
+              StreakTracker
+            </p>
+          </div>
+          <div className="">
+            <button className="btn btn-ghost btn-circle text-success">
+              Add
+            </button>
+            <button className="btn btn-ghost btn-circle text-warning">
+              Edit
+            </button>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
