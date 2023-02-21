ad45905d9f00cab30038502f2830e11d8cd54228
Karl Cereno <kagc.dev@gmail.com>
2023-02-21 13:26:16 -0500

    AddHabit modal draft

--- src/layout/Layout/Modals/Modal.tsx
+++ src/layout/Layout/Modals/Modal.tsx
@@ -4,13 +4,14 @@ import React from 'react';
 function Modal() {
   return (
     <>
-      <input type="checkbox" id="modal" className="modal-toggle" />
+      {/*  */}
+      <input type="checkbox" id="" className="modal-toggle" />
       <div className="modal">
-        <div className="modal-box">
+        <div className="modal-box w-72 max-w-5xl">
           <h3 className="font-bold text-lg">New Habit</h3>
           <div className="mt-4">
             <form action="">
-              <div className="form-control w-full max-w-xs">
+              <div className="form-control  max-w-xs">
                 <label className="label">
                   <span className="label-text">What do you want to do?</span>
                 </label>
@@ -43,8 +44,7 @@ function Modal() {
             </form>
           </div>
           <div className="modal-action">
-            {/* Closes Modal */}
-            <label htmlFor="modal" className="btn">
+            <label htmlFor="my-modal-5" className="btn">
               Yay!
             </label>
           </div>
