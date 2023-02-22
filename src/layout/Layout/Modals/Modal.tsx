3d3e1c730d4286c347bdf1ba1298cf4e5c2e68e6
Karl Cereno <kagc.dev@gmail.com>
2023-02-21 23:07:29 -0500

    Finish add habit form

--- src/layout/Layout/Modals/Modal.tsx
+++ src/layout/Layout/Modals/Modal.tsx
@@ -1,64 +1,57 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React from 'react';
-import EmojiPicker, { Theme } from 'emoji-picker-react';
+import { useState } from 'react';
 
 function Modal() {
   return (
-    <>
+    <div>
       {/*  */}
       <input type="checkbox" id="modal" className="modal-toggle" />
+
       <div className="modal">
         <div className="modal-box">
-          <form action="">
+          <form>
             <div className="form-control ">
               <label className="label">
                 <span className="label-text">What do you want to do?</span>
               </label>
-              <input
-                type="text"
-                placeholder="Exercise"
-                className="input input-bordered w-full "
-              />
+              <input type="text" className="input input-bordered w-full " />
             </div>
             <div className="form-control w-full mt-2">
               <label className="label">
                 <span className="label-text">How many time a day?</span>
               </label>
-              <input
-                type="number"
-                placeholder="1"
-                className="input input-bordered w-full"
-              />
+              <input type="number" className="input input-bordered w-full" />
             </div>
             <div className="form-control w-full mt-2">
-              <div>
-                <label className="label">
-                  <span className="label-text">Select an emoji as an icon</span>
-                </label>
-                <EmojiPicker
-                  theme={Theme.DARK}
-                  width="100%"
-                  previewConfig={{
-                    defaultCaption: 'Pick one!',
-                    defaultEmoji: '1f92a', // 🤪
-                  }}
-                />
-              </div>
+              <label className="label">
+                <span className="label-text">
+                  Enter an emoji or letter to use as an icon
+                </span>
+              </label>
+              <input type="string" className="input input-bordered w-full" />
+            </div>
+            <span className="label-text-alt">
+              To acces the emoji keyboard, cick on the input field and press:
+              <br />
+              On PCs: Press Windows key + ; (semicolon) or Windows key +.
+              (period)
+              <br />
+              On Macs: Press Command + Control + Space
+            </span>
+            <div className="modal-action">
+              <button type="button" className="btn btn-success">
+                Add Habit
+              </button>
+              <button type="button" className="btn btn-outline btn-error">
+                <label htmlFor="modal">Cancel</label>
+              </button>
             </div>
           </form>
-
-          <div className="modal-action">
-            <button type="button" className="btn btn-success">
-              Add Habit
-            </button>
-            <button type="button" className="btn btn-outline btn-error">
-              <label htmlFor="modal">Cancel</label>
-            </button>
-          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
