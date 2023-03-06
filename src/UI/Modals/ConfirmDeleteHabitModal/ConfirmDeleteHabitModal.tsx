edc832d8dc956afeee342c458b752bb718e2f770
Karl Cereno <kagc.dev@gmail.com>
2023-03-05 23:32:55 -0500

    Fix delte modal for mobile

--- src/UI/Modals/ConfirmDeleteHabitModal/ConfirmDeleteHabitModal.tsx
+++ src/UI/Modals/ConfirmDeleteHabitModal/ConfirmDeleteHabitModal.tsx
@@ -15,18 +15,17 @@ function ConfirmDeleteHabitModal() {
     <div className={`modal ${modalOpen && modalType === 'delete' && 'modal-open'}`}>
       <div className="modal-box">
         <h3 className="font-bold text-lg">Warning!</h3>
-        <p className="py-4">
-          Are you you want to delete this habit?
-          <br />
-          This action cannot be undone and all data will be erased.
-        </p>
-        <div className="modal-action">
-          <button type="button" className="btn btn-outline" onClick={handleDeleteButtonClick}>
-            No, I changed my mind
-          </button>
-          <button type="button" className="btn btn-error" onClick={handleDeleteButtonClick}>
-            Yes, delete it
-          </button>
+        <p className="py-4">Are you you want to delete this habit?</p>
+        <p> This action cannot be undone and all data will be erased.</p>
+        <div className="mt-6">
+          <div className="flex flex-col gap-2 align-center mobile-large:flex-row mobile-large:justify-end">
+            <button type="button" className="btn btn-outline" onClick={handleDeleteButtonClick}>
+              No, I changed my mind
+            </button>
+            <button type="button" className="btn btn-error" onClick={handleDeleteButtonClick}>
+              Yes, delete it
+            </button>
+          </div>
         </div>
       </div>
     </div>
